758102eba8adebbfd44970aa0600c23cc144a718
Alba Gonzalez <albatgonzalezm@gmail.com>
2020-04-07 18:44:43 +0100

    feat: Add insert, delete, update methods to Models

--- app.py
+++ app.py
@@ -1,14 +1,46 @@
 import os
 from flask import Flask, redirect, url_for
 from flask_cors import CORS
-from models import setup_db
+from datetime import datetime
+from models import setup_db, Restaurant, Review
 
-def create_app(test_config=None):
+def create_app(test_config = None):
 
     app = Flask(__name__)
     setup_db(app)
     # Allow CORS for all domains on all routes
     CORS(app)
+
+    # Insert records so we can test our class methods and db
+    chinese = Restaurant(
+        name = "Mission Chinese Food",
+        borough = "Manhattan",
+        photograph = "1.jpg",
+        img_description = "An inside view of a busy restaurant with all tables ocupied by people enjoying their meal",
+        address = "171 E Broadway, New York, NY 10002",
+        latlng = str({
+          "lat": 40.713829,
+          "lng": -73.989667
+        }),
+        cuisine = "Asian",
+        operating_hours = str({
+          "Monday": "5:30 pm - 11:00 pm",
+          "Tuesday": "5:30 pm - 12:00 am",
+          "Wednesday": "5:30 pm - 12:00 am",
+          "Thursday": "5:30 pm - 12:00 am",
+          "Friday": "5:30 pm - 12:00 am",
+          "Saturday": "12:00 pm - 4:00 pm, 5:30 pm - 12:00 am",
+          "Sunday": "12:00 pm - 4:00 pm, 5:30 pm - 11:00 pm"
+        }))
+    chinese.insert()
+
+    my_review = Review(
+        restaurant_id = 1,
+        name = "Morgan",
+        date = datetime.today(),
+        rating = 4,
+        comments = "This place is a blast.")
+    my_review.insert()
 
     @app.route('/')
     def index():
--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 import os
-from sqlalchemy import Column, String, Integer, Date, create_engine
+from sqlalchemy import Column, String, Integer, DateTime, create_engine
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 import json
 
 database_path = os.environ['DATABASE_URL']
@@ -9,35 +10,41 @@ db = SQLAlchemy()
 
 '''
 setup_db(app)
-    binds a flask application and a SQLAlchemy service
+    Binds a flask application and a SQLAlchemy service.
 '''
-def setup_db(app, database_path=database_path):
+def setup_db(app, database_path = database_path):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
+
+    '''
+    Drop the database tables and start fresh
+    Initialize a clean database
+    '''
     db.drop_all()
     db.create_all()
 
 
 '''
 Restaurants
-This table holds the information of all participating restaurants
+    This table holds the information of all participating restaurants.
 '''
 class Restaurant(db.Model):
     __tablename__ = 'Restaurants'
 
-    id = Column(Integer, primary_key=True)
-    name = Column(String, nullable=False)
-    borough = Column(String, nullable=False)
-    photograph = Column(String, nullable=False)
-    img_description = Column(String, nullable=False)
-    address = Column(String, nullable=False)
-    latlng = Column(String, nullable=False)
-    cuisine = Column(String, nullable=False)
-    operating_hours = Column(String, nullable=False)
+    id = Column(Integer, primary_key = True)
+    name = Column(String, nullable = False, unique = True)
+    borough = Column(String, nullable = False)
+    photograph = Column(String, nullable = False)
+    img_description = Column(String, nullable = False)
+    address = Column(String, nullable = False)
+    latlng = Column(String, nullable = False)
+    cuisine = Column(String, nullable = False)
+    operating_hours = Column(String, nullable = False)
 
-    def __init__(self, name, borough, photograph, img_description, address, latlng, cuisine, operating_hours):
+    def __init__(self, name, borough, photograph, img_description, address,
+                 latlng, cuisine, operating_hours):
         self.name = name
         self.borough = borough
         self.photograph = photograph
@@ -60,19 +67,74 @@ class Restaurant(db.Model):
             'operating_hours': self.operating_hours
         }
 
+    '''
+    insert()
+        Inserts a new model into a database.
+        The model must have a unique name.
+        EXAMPLE
+            chinese = Restaurant(
+                name = "Mission Chinese Food",
+                borough = "Manhattan",
+                photograph = "1.jpg",
+                img_description = "An inside view",
+                address = "171 E Broadway, New York, NY 10002",
+                latlng = str({
+                  "lat": 40.713829,
+                  "lng": -73.989667
+                }),
+                cuisine = "Asian",
+                operating_hours = str({
+                  "Monday": "5:30 pm - 11:00 pm",
+                  "Tuesday": "5:30 pm - 12:00 am",
+                  "Wednesday": "5:30 pm - 12:00 am",
+                  "Thursday": "5:30 pm - 12:00 am",
+                  "Friday": "5:30 pm - 12:00 am",
+                  "Saturday": "12:00 pm - 4:00 pm, 5:30 pm - 12:00 am",
+                  "Sunday": "12:00 pm - 4:00 pm, 5:30 pm - 11:00 pm"
+                }))
+            chinese.insert()
+    '''
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    '''
+    delete()
+        Deletes a model from a database.
+        The model must exist in the database.
+        EXAMPLE
+            chinese.insert()
+            chinese.delete()
+    '''
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    '''
+    update()
+        Updates a model into a database.
+        The model must exist in the database.
+        EXAMPLE
+            chinese = Restaurant.query.filter(Restaurant.id == id).one_or_none()
+            chinese.name = "Local Chinese"
+            chinese.update()
+    '''
+    def update(self):
+        db.session.commit()
+
 '''
 Reviews
-This table holds the information of all the reviews made to all restaurants
+    This table holds the information of all the reviews made to all restaurants.
 '''
 class Review(db.Model):
     __tablename__ = 'Reviews'
 
-    id = Column(Integer, primary_key=True)
-    restaurant_id = Column(Integer)
-    name = Column(String, nullable=False)
-    date = Column(Date, nullable=False)
-    rating = Column(Integer, nullable=False)
-    comments = Column(String, nullable=False)
+    id = Column(Integer, primary_key = True)
+    restaurant_id = Column(Integer, nullable = False)
+    name = Column(String, nullable = False)
+    date = Column(DateTime, nullable = False, default = datetime.today())
+    rating = Column(Integer, nullable = False)
+    comments = Column(String, nullable = False)
 
     def __init__(self, restaurant_id, name, date, rating, comments):
         self.restaurant_id = restaurant_id
@@ -90,3 +152,30 @@ class Review(db.Model):
             'rating': self.rating,
             'comments': self.comments
         }
+
+    '''
+    insert()
+        Inserts a new model into a database.
+        EXAMPLE
+            my_review = Review(
+                restaurant_id = 1,
+                name = "Morgan",
+                rating = 4,
+                comments = "This place is a blast.")
+            my_review.insert()
+    '''
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    '''
+    delete()
+        Deletes a model from a database.
+        The model must exist in the database.
+        EXAMPLE
+            my_review.insert()
+            my_review.delete()
+    '''
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
