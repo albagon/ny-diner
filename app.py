1d946ac26892304d5af9f0c0b02c19f0accc56cd
Alba Gonzalez <albatgonzalezm@gmail.com>
2020-04-28 04:39:42 +0100

    feat: Add error handler for 500 error code

--- app.py
+++ app.py
@@ -330,6 +330,14 @@ def create_app(test_config = None):
                         "message": "unprocessable"
                         }), 422
 
+    @app.errorhandler(500)
+    def server_error(error):
+        return jsonify({
+                        "success": False,
+                        "error": 500,
+                        "message": "internal server error"
+                        }), 500
+
     return app
 
 app = create_app()
